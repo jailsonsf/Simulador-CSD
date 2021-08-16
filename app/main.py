81d884636f2f31ad7b5ac6c58859b173a94fb330
jailsonsf <jailson.s.f@outlook.com>
2021-08-16 11:17:49 -0300

    feat: update project
    Add unit converter
    Refactoring app

--- app/main.py
+++ app/main.py
@@ -1,40 +1,50 @@
 from time import sleep
-from utils import clear
+from app.utils import clear
 
-while True:
-    clear()
+from app.unit_converter.unit_converter import UnitConverter
 
-    print(f'Digite [0] para Sair')
-    print(f'Digite [1] para converter unidades')
-    print(f'Digite [2] para converter bases')
-    print(f'Digite [3] para realizar soma binária')
-    print(f'Digite [4] para realizar operações de portas lógicas')
-    print(f'Digite [5] para analisar expressão da álgebra relacional')
-    option = int(input('Selecione uma opção: '))
 
-    if option == 0:
-        print('Saindo...')
-        break
-    elif option == 1:
-        print(f'Opção {option}: Conversão de unidades de medida')
-        sleep(1)
+class App:
+    def __init__(self) -> None:
+        clear()
+        while True:
+            print('='*40)
+            print(f'Digite [0] para Sair')
+            print(f'Digite [1] para converter unidades')
+            print(f'Digite [2] para converter bases')
+            print(f'Digite [3] para realizar soma binária')
+            print(f'Digite [4] para realizar operações de portas lógicas')
+            print(f'Digite [5] para analisar expressão da álgebra relacional')
+            option = int(input('>> '))
 
-    elif option == 2:
-        print(f'Opção {option}: Conversão de bases numéricas')
-        sleep(1)
+            if option == 0:
+                print('Saindo...')
+                sleep(1)
+                clear()
+                break
+            elif option == 1:
+                print(f'Opção [{option}]: Conversão de unidades de medida')
+                converter = UnitConverter()
 
-    elif option == 3:
-        print(f'Opção {option}: Soma binária')
-        sleep(1)
+                sleep(1)
 
-    elif option == 4:
-        print(f'Opção {option}: Operações com portas lógicas')
-        sleep(1)
+            elif option == 2:
+                print(f'Opção [{option}]: Conversão de bases numéricas')
+                sleep(1)
 
-    elif option == 5:
-        print(f'Opção {option}: Analisar expressões da álgebra relacional')
-        sleep(1)
+            elif option == 3:
+                print(f'Opção [{option}]: Soma binária')
+                sleep(1)
 
-    else:
-        print(f'Opção inexistente!')
-        sleep(1)
+            elif option == 4:
+                print(f'Opção [{option}]: Operações com portas lógicas')
+                sleep(1)
+
+            elif option == 5:
+                print(
+                    f'Opção [{option}]: Analisar expressões da álgebra relacional')
+                sleep(1)
+
+            else:
+                print(f'Opção inexistente!')
+                sleep(1)
---
+++ app/unit_converter/unit_converter.py
@@ -0,0 +1,73 @@
+units = {
+    1: 'Byte',
+    2: 'Kylobyte',
+    3: 'Megabyte',
+    4: 'Gigabyte',
+    5: 'Terabyte'
+}
+
+
+class UnitConverter:
+    def __init__(self) -> None:
+        self.get_unit_to_provide()
+        self.get_unit_to_converter()
+        self.get_value()
+        self.converter()
+
+    def show_units(self) -> None:
+        for key in units:
+            print(f'[{key}] - {units[key]}')
+
+    def get_unit_to_provide(self) -> None:
+        print(f'Selecione a unidade de medida para qual fornecerá um valor:')
+        self.show_units()
+
+        option = int(input('>> '))
+
+        if option not in units.keys():
+            print(f'Opção inválida!')
+
+        else:
+            self.unit_provide = units[option]
+
+    def get_unit_to_converter(self) -> None:
+        print(f'Selecione a unidade de medida para qual deseja converter:')
+        self.show_units()
+
+        option = int(input('>> '))
+
+        if option not in units.keys():
+            print(f'Opção inválida!')
+
+        elif option == self.unit_provide:
+            print(f'Escolha uma unidade diferente para fazer a converção!')
+
+        else:
+            self.unit_converter = units[option]
+
+    def get_value(self) -> None:
+        print(f'Digite o valor em {self.unit_provide}:')
+        self.value = float(input('>> '))
+
+    def converter(self) -> None:
+        for key in units:
+            if units[key] == self.unit_provide:
+                unit_provide_num = key
+
+            elif units[key] == self.unit_converter:
+                unit_converter_num = key
+
+        if self.unit_provide == self.unit_converter:
+            print(
+                f'{self.value} {self.unit_provide} equivalem a {self.value} {self.unit_converter}')
+
+        elif unit_provide_num > unit_converter_num:
+            multiplications = abs(unit_provide_num - unit_converter_num)
+
+            print(
+                f'{self.value} {self.unit_provide} equivalem a {self.value * (1000**multiplications)} {self.unit_converter}')
+
+        elif unit_provide_num < unit_converter_num:
+            divisions = abs(unit_provide_num - unit_converter_num)
+            print(
+                f'{self.value} {self.unit_provide} equivalem a {self.value / (1000**divisions)} {self.unit_converter}')
